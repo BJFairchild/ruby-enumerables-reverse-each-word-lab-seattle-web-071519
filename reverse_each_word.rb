731d34495441c4c7f320913ddd4340a97b2a6329
Brittan Fairchild <brittanfairchild@gmail.com>
2019-07-12 22:39:19 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,6 @@ def reverse_each_word(string)
   string = string.split(" ")
   i = 0
   
-  #while(i < string.length)
-    #string.each do |string|
-     #string = string.reverse()
+  while(i < string.length)
+    string.each do |string|
+     string = string.reverse()
